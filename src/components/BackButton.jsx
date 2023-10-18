4a174abd0b0a3fecc3fd2ec823d4b0d3ff02784f
Enoh Collins <eno.col@gmail.com>
2023-10-18 13:37:38 +0100

    Add and edit BackButton and Home components

--- src/components/BackButton.jsx
+++ src/components/BackButton.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 const BackButton = () => {
   return (
     <div className="backButton-container">
-      <Link to="/">Back</Link>
+      <Link to="/">
+        <i class="fa-solid fa-less-than fa-lg"></i>
+      </Link>
     </div>
   );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchcategories } from '../redux/CategoriesSlice';
+import Header from './Header';
 
 const Home = () => {
   const category = useSelector((state) => state.category.category.data);
@@ -15,10 +16,19 @@ const Home = () => {
   if (category)
     return (
       <div className="home">
+        <Header />
         <ul className="category-list">
           {category.map((m) => (
-            <Link to={`/games/${m.id}`} key={m.id}>
-              <li>{m.home_team.full_name}</li>
+            <Link to={`/games/${m.id}`} key={m.id} className="link">
+              <li className="list">
+                <div className="icon">
+                  <i className="fa-solid fa-arrow-up fa-rotate-90 fa-xs"></i>
+                </div>
+                <div className="container">
+                  <div className="name">{m.home_team.full_name}</div>
+                  <div>Home score: {m.home_team_score}</div>
+                </div>
+              </li>
             </Link>
           ))}
         </ul>
