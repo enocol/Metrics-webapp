0e02bb7f98d568725a8807187a393561a3b95739
Enoh Collins <eno.col@gmail.com>
2023-10-18 13:34:53 +0100

    Add Header.jsx component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="header">
+      <i className="fa-solid fa-house"></i>
+
+      <div className="title">Ball Don't lie Team Stats</div>
+    </div>
+  );
+};
+
+export default Header;
