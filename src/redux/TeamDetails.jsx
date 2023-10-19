8353aa5e8c425cda8fd48b45b31ef141d5a416a5
Enoh Collins <eno.col@gmail.com>
2023-10-19 04:56:51 +0100

    Edit TeamDetails.js

--- src/redux/TeamDetails.jsx
+++ src/redux/TeamDetails.jsx
@@ -40,23 +40,23 @@ const TeamDetails = () => {
         <p className="team-name">{gameData.home_team.full_name}</p>
         <div className="flex-items">
           <span className="data">Home Score:</span>
-          {' '}
+
           {gameData.home_team_score}
         </div>
 
         <div className="flex-items">
           <span className="data">Season:</span>
-          {' '}
+
           {gameData.season}
         </div>
         <div className="flex-items">
           <span className="data">Period:</span>
-          {' '}
+
           {gameData.period}
         </div>
         <div className="flex-items">
           <span className="data">Visitor Team Score:</span>
-          {' '}
+
           {gameData.visitor_team_score}
         </div>
       </div>
@@ -65,3 +65,4 @@ const TeamDetails = () => {
 };
 
 export default TeamDetails;
+
