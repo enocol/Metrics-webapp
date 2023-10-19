f391aded5d3114acf0a4e4b7fc04bca554597e44
Enoh Collins <eno.col@gmail.com>
2023-10-19 04:49:13 +0100

    Edit Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,26 +11,22 @@ const Home = () => {
 
   useEffect(() => {
     if (category) dispatch(fetchcategories());
-  }, [dispatch, category]);
+  }, []);
 
   if (category) {
     return (
-      <div className="home">
+      <div>
         <Header />
         <ul className="category-list">
           {category.map((m) => (
             <Link to={`/games/${m.id}`} key={m.id} className="link">
               <li className="list">
-                <div className="icon">
-                  <i className="fa-solid fa-arrow-up fa-rotate-90 fa-xs" />
-                </div>
-                <div className="container">
-                  <div className="name">{m.home_team.full_name}</div>
-                  <div>
-                    Home score:
-                    {m.home_team_score}
-                  </div>
-                </div>
+                <i className="fa-solid fa-arrow-up fa-rotate-90 fa-xs icon" />
+                <article className="full-name">{m.home_team.full_name}</article>
+                <article>
+                  Home score:
+                  {m.home_team_score}
+                </article>
               </li>
             </Link>
           ))}
