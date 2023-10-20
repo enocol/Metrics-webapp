23312d4be55e9fb7fe34930ded281422ac95362d
Enoh Collins <eno.col@gmail.com>
2023-10-20 10:39:56 +0100

    Edit header.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,22 @@
 import { useSelector } from 'react-redux/es/hooks/useSelector';
+import image from '../images/map.png';
 
 const Header = () => {
   const category = useSelector((state) => state.category.category.data);
 
   return (
     <div className="header">
-      <h1 className="title">America</h1>
-
-      <div className="season">{category[0].season}</div>
+      <div className="icon-container">
+        <i className="fa-solid fa-microphone fa-sm icon1"></i>
+        <i className="fa-solid fa-gear fa-sm icon2"></i>
+      </div>
+      <div className="season-container">
+        <h1 className="title">America</h1>
+        <div className="season">{category[0].season}</div>
+      </div>
     </div>
   );
 };
 
 export default Header;
+
